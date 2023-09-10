424054ead0d044aaaa841645cca7b0aaf8e29f8f
aditiya359542 <adityaseth26052002@gmail.com>
2023-09-10 10:21:10 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+gsap.from('.logo,.links a,.navbtn,.menu', {
+    y: -50,
+    opacity: 0,
+    delay: 0.4,
+    duration: 0.6,
+    stagger: 0.2
+
+});
+
+
+gsap.from('.dogs', {
+    y: 40,
+    opacity: 0,
+    delay: 0.6,
+    duration: 1.8,
+    stagger: 0.4
+})
+
+gsap.from('.contect h1 ,.contect p', {
+    y: 50,
+    opacity: 0,
+    delay: 1.5,
+    duration: 0.7,
+    stagger: 0.8,
+})
+
+gsap.from('.cards , .cards_list', {
+    y: 50,
+    opacity: 0,
+    delay: 1.5,
+    duration: 0.7,
+    stagger: 0.8,
+})
+
+
+
